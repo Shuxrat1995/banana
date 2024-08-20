d3ce78ba45bfc20231ebf2a855f58505f7fdc71d
Shuxrat <shuxratturaev96@gmail.com>
2024-08-20 10:13:56 +0500

    banana

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+let banana =  prompt ('Введите колличество бананов')
+
+for(let i = 1; i < banana;i++) {
+         if (i == 1) {
+             console.log(i  + ' banana '); 
+         }else {
+             console.log(i + ' bananas ');
+}
+}
+
+    
+
+
+
